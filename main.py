d4a0d9a9d14152ee3a9f4f464d58aa6f84b23623
Pasu Sangiemsin <opxz7148@gmail.com>
2024-02-27 17:44:42 +0700

    Refactor dependencies injection

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
  
     # Create a unit converter and give it to the ConverterUI
     # via the constructor. This is called Dependency Injection.
-    ui = ConverterUI(UnitConverter)
+    ui = ConverterUI(UnitConverter())
     ui.run()
