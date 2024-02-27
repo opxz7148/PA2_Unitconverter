77d4262b240cda2bf86c7778df7db7082093a54c
Pasu Sangiemsin <opxz7148@gmail.com>
2024-02-27 17:54:40 +0700

    Refactor code move some component setting to init_component instead of __init__

--- converter_ui.py
+++ converter_ui.py
@@ -15,39 +15,18 @@ class ConverterUI(tk.Tk):
         self.converter = converter
 
         # Setting up menubar
-        menubar = tk.Menu(self)
-        self.config(menu=menubar)
+        self.menubar = tk.Menu(self)
+        self.config(menu=self.menubar)
 
         self.unit = tk.StringVar()
 
         # Create all component for GUI
         self.l_combo = FieldCombo(self)
         self.r_combo = FieldCombo(self)
-        self.label = tk.Label(self, text=" = ")
-        self.convert_button = tk.Button(self, text="convert", command=self.convert_handler)
-        self.clear_button = tk.Button(self, text="clear", command=self.clear_handler)
 
-        # Setting up unit menu
-        unit_list = tk.Menu(menubar)
-
-        # Load all unittype
-        unittype = self.converter.get_unit_type()
-
-        for unit in unittype:
-            unit_list.add_radiobutton(
-                label=unit.value[0],
-                value=unit.value[0],
-                variable=self.unit,
-                command=self.change_unit
-            )
-
-        unit_list.add_radiobutton(label="Exit", command=self.destroy)
-
-        menubar.add_cascade(
-            label="unit",
-            menu=unit_list,
-        )
-        self.use_unit = unittype[0]
+        # Get all unittype
+        self.unittype = self.converter.get_unit_type()
+        self.use_unit = self.unittype[0]
 
         # Call init_component to set thing up
         self.init_components()
@@ -57,6 +36,29 @@ class ConverterUI(tk.Tk):
         # use a frame as internal container
         # this will help later when the UI gets more complex
 
+        label = tk.Label(self, text=" = ")
+        convert_button = tk.Button(self, text="convert", command=self.convert_handler)
+        clear_button = tk.Button(self, text="clear", command=self.clear_handler)
+
+        # Setting up unit menu
+        unit_list = tk.Menu(self.menubar)
+
+        # Load all unittype to menu
+        for unit in self.unittype:
+            unit_list.add_radiobutton(
+                label=unit.value[0],
+                value=unit.value[0],
+                variable=self.unit,
+                command=self.change_unit
+            )
+
+        unit_list.add_radiobutton(label="Exit", command=self.destroy)
+
+        self.menubar.add_cascade(
+            label="unit",
+            menu=unit_list,
+        )
+
         # Set window title.
         self.title("Converter")
 
@@ -65,10 +67,10 @@ class ConverterUI(tk.Tk):
 
         # Place component on window.
         self.l_combo.set_to_the_left()
-        self.label.pack(side=tk.LEFT)
+        label.pack(side=tk.LEFT)
         self.r_combo.set_to_the_left()
-        self.convert_button.pack(side=tk.LEFT)
-        self.clear_button.pack(side=tk.LEFT)
+        convert_button.pack(side=tk.LEFT)
+        clear_button.pack(side=tk.LEFT)
 
         # Binding function
         self.l_combo.field.bind('<Return>', self.convert_handler)
